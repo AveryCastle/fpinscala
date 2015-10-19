fcb294c33a614c2644db636979a7b3dce92ca580
JuHyun Seong <ernestogevar@naver.com>
2015-10-19 21:46:31 +0900

    예제 3.6까지 실습

--- src/main/scala/chap3.scala
+++ src/main/scala/chap3.scala
@@ -35,10 +35,19 @@ object List {
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil 
     case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l 
-  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  } 
 }
 
+case class Point(x: Int, y: Int) 
+
 object Test {
+
+  // TODO: Test 코드로 변경하여 테스트하기
   def main(args: Array[String]) {  
     val list: List[Int] = List(1, 2, 3, 4, 5)
     println(list)
@@ -50,5 +59,9 @@ object Test {
     println(List.drop(list, 10)) 
     println(List.dropWhile(list, (x: Int) => x < 4)) 
     println(List.dropWhile(list, (x: Int) => x < 10)) 
+
+    val point = Point(3, 2)
+
+    println(List.init(list))
   }
 }
